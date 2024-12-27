16e059840cefdea329455ecc8707f81e7421b7f7
wagneruchoa <32679900+wagneruchoa@users.noreply.github.com>
2024-12-27 09:15:37 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.WebHost.UseUrls("http://*:8080");
+builder.WebHost.UseUrls("http://*:5000");
 
 var app = builder.Build();
 
